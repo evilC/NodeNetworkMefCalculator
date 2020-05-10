b66f4cac4a7f327dc76e973febfa04947f09d5a6
Clive Galway <evilc@evilc.com>
2020-05-10 15:38:16 +0100

    Initial commit

---
+++ MefCalculator.Gui/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using DynamicData;
+using NodeNetwork.ViewModels;
+
+namespace MefCalculator.Gui
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Create a new viewmodel for the NetworkView
+            var network = new NetworkViewModel();
+
+            //Create the node for the first node, set its name and add it to the network.
+            var node1 = new NodeViewModel();
+            node1.Name = "Node 1";
+            network.Nodes.Add(node1);
+
+            //Create the viewmodel for the input on the first node, set its name and add it to the node.
+            var node1Input = new NodeInputViewModel();
+            node1Input.Name = "Node 1 input";
+            node1.Inputs.Add(node1Input);
+
+            //Create the second node viewmodel, set its name, add it to the network and add an output in a similar fashion.
+            var node2 = new NodeViewModel();
+            node2.Name = "Node 2";
+            network.Nodes.Add(node2);
+
+            var node2Output = new NodeOutputViewModel();
+            node2Output.Name = "Node 2 output";
+            node2.Outputs.Add(node2Output);
+
+            //Assign the viewmodel to the view.
+            networkView.ViewModel = network;
+        }
+    }
+}
